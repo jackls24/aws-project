870eed32e3301a6e11276f62a411c2db045fdc8c
jackls24 <giacomo1000@live.it>
2025-06-28 19:26:32 +0200

    add changes 16

--- backend/src/main.py
+++ backend/src/main.py
@@ -22,7 +22,7 @@ app.add_middleware(
 
 @app.get("/")
 async def root():
-    return {"message": "AWS Backend API is running with FastAPI changed BY ACTION By Action NEWWW   NEW 15"}
+    return {"message": "AWS Backend API is running with FastAPI changed BY ACTION By Action NEWWW   NEW 16"}
 
 @app.get("/health")
 async def health_check():
